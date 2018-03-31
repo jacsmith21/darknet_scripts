ba311176d88771b8290c2cae33d1be66121d4321
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-03-31 20:20:27 -0300

    converted gen_anchors.py to python3, refactored parts of file to pep8 standards

--- gen_anchors.py
+++ gen_anchors.py
@@ -1,165 +1,157 @@
-'''
+"""
 Created on Feb 20, 2017
 
 @author: jumabek
-'''
-from os import listdir
-from os.path import isfile, join
+"""
 import argparse
-import cv2
 import numpy as np
-import sys
 import os
-import shutil
-import random 
-import math
+import random
 
 width_in_cfg_file = 416.
 height_in_cfg_file = 416.
 
-def IOU(x,centroids):
+
+def iou(x, centroids):
     similarities = []
-    k = len(centroids)
     for centroid in centroids:
-        c_w,c_h = centroid
-        w,h = x
-        if c_w>=w and c_h>=h:
-            similarity = w*h/(c_w*c_h)
-        elif c_w>=w and c_h<=h:
+        c_w, c_h = centroid
+        w, h = x
+        if c_w >= w and c_h >= h:
+            similarity = w*h / (c_w*c_h)
+        elif c_w >= w and c_h <= h:
             similarity = w*c_h/(w*h + (c_w-w)*c_h)
-        elif c_w<=w and c_h>=h:
+        elif c_w <= w and c_h >= h:
             similarity = c_w*h/(w*h + c_w*(c_h-h))
-        else: #means both w,h are bigger than c_w and c_h respectively
+        else:  # means both w,h are bigger than c_w and c_h respectively
             similarity = (c_w*c_h)/(w*h)
-        similarities.append(similarity) # will become (k,) shape
+        similarities.append(similarity)  # will become (k,) shape
     return np.array(similarities) 
 
-def avg_IOU(X,centroids):
-    n,d = X.shape
-    sum = 0.
-    for i in range(X.shape[0]):
-        #note IOU() will return array which contains IoU for each centroid and X[i] // slightly ineffective, but I am too lazy
-        sum+= max(IOU(X[i],centroids)) 
-    return sum/n
 
-def write_anchors_to_file(centroids,X,anchor_file):
-    f = open(anchor_file,'w')
+def avg_iou(annotation_dims, centroids):
+    n_annotations, _ = annotation_dims.shape
+    total = 0.
+    for i in range(annotation_dims.shape[0]):
+        # note IOU() will return array which contains IoU for each centroid and X[i] // slightly ineffective,
+        # but I am too lazy
+        total += max(iou(annotation_dims[i], centroids))
+    return total / n_annotations
+
+
+def write_anchors_to_file(centroids, annotation_dims, anchor_file):
+    f = open(anchor_file, 'w')
     
     anchors = centroids.copy()
-    print anchors.shape
+    print(anchors.shape)
 
     for i in range(anchors.shape[0]):
-        anchors[i][0]*=width_in_cfg_file/32.
-        anchors[i][1]*=height_in_cfg_file/32.
-         
+        anchors[i][0] *= width_in_cfg_file / 32
+        anchors[i][1] *= height_in_cfg_file / 32
 
-    widths = anchors[:,0]
+    widths = anchors[:, 0]
     sorted_indices = np.argsort(widths)
 
-    print 'Anchors = ', anchors[sorted_indices] 
+    print('Anchors = ', anchors[sorted_indices])
         
     for i in sorted_indices[:-1]:
-        f.write('%0.2f,%0.2f, '%(anchors[i,0],anchors[i,1]))
+        f.write('%0.2f,%0.2f, ' % (anchors[i, 0], anchors[i, 1]))
 
-    #there should not be comma after last anchor, that's why
-    f.write('%0.2f,%0.2f\n'%(anchors[sorted_indices[-1:],0],anchors[sorted_indices[-1:],1]))
+    # there should not be comma after last anchor, that's why
+    f.write('%0.2f,%0.2f\n' % (anchors[sorted_indices[-1:], 0], anchors[sorted_indices[-1:], 1]))
     
-    f.write('%f\n'%(avg_IOU(X,centroids)))
-    print
+    f.write('%f\n' % (avg_iou(annotation_dims, centroids)))
+    print()
 
-def kmeans(X,centroids,eps,anchor_file):
-    
-    N = X.shape[0]
-    iterations = 0
-    k,dim = centroids.shape
-    prev_assignments = np.ones(N)*(-1)    
-    iter = 0
-    old_D = np.zeros((N,k))
 
+def kmeans(annotation_dims, centroids, anchor_file):
+    num_annotations = annotation_dims.shape[0]
+    k, dim = centroids.shape
+    prev_assignments = np.ones(num_annotations)*(-1)
+    old_distance = np.zeros((num_annotations, k))
+
+    count = 0
     while True:
-        D = [] 
-        iter+=1           
-        for i in range(N):
-            d = 1 - IOU(X[i],centroids)
-            D.append(d)
-        D = np.array(D) # D.shape = (N,k)
-        
-        print "iter {}: dists = {}".format(iter,np.sum(np.abs(old_D-D)))
-            
-        #assign samples to centroids 
-        assignments = np.argmin(D,axis=1)
-        
-        if (assignments == prev_assignments).all() :
-            print "Centroids = ",centroids            
-            write_anchors_to_file(centroids,X,anchor_file)
+        distances = []
+        for i in range(num_annotations):
+            distance = 1 - iou(annotation_dims[i], centroids)
+            distances.append(distance)
+        distances = np.array(distances)  # shape: (N,k)
+
+        print("iter {}: dists = {}".format(count, np.sum(np.abs(old_distance-distances))))
+
+        # assign samples to centroids
+        assignments = np.argmin(distances, axis=1)
+
+        if (assignments == prev_assignments).all():
+            print("Centroids = ", centroids)
+            write_anchors_to_file(centroids, annotation_dims, anchor_file)
             return
 
-        #calculate new centroids
-        centroid_sums=np.zeros((k,dim),np.float)
-        for i in range(N):
-            centroid_sums[assignments[i]]+=X[i]        
-        for j in range(k):            
-            centroids[j] = centroid_sums[j]/(np.sum(assignments==j))
-        
-        prev_assignments = assignments.copy()     
-        old_D = D.copy()  
+        # calculate new centroids
+        centroid_sums = np.zeros((k, dim), np.float)
+        for i in range(num_annotations):
+            centroid_sums[assignments[i]] += annotation_dims[i]
+        for j in range(k):
+            centroids[j] = centroid_sums[j]/(np.sum(assignments == j))
 
-def main(argv):
+        prev_assignments = assignments.copy()
+        old_distance = distances.copy()
+
+        count += 1
+
+
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-filelist', default = '\\path\\to\\voc\\filelist\\train.txt', 
-                        help='path to filelist\n' )
-    parser.add_argument('-output_dir', default = 'generated_anchors/anchors', type = str, 
-                        help='Output anchor directory\n' )  
-    parser.add_argument('-num_clusters', default = 0, type = int, 
-                        help='number of clusters\n' )  
+    parser.add_argument('-filelist', default='\\path\\to\\voc\\filelist\\train.txt',
+                        help='path to filelist\n')
+    parser.add_argument('-output_dir', default='generated_anchors/anchors', type=str,
+                        help='Output anchor directory\n')
+    parser.add_argument('-num_clusters', default=0, type=int,
+                        help='number of clusters\n')
 
-   
     args = parser.parse_args()
     
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
-    f = open(args.filelist)
+    image_list = open(args.filelist)
   
-    lines = [line.rstrip('\n') for line in f.readlines()]
+    lines = [image_path.rstrip('\n') for image_path in image_list.readlines()]
     
     annotation_dims = []
 
-    size = np.zeros((1,1,3))
-    for line in lines:
+    for image_path in lines:
                     
-        line = line.replace('images','labels')
-        line = line.replace('img1','labels')
-        line = line.replace('JPEGImages','labels')        
-        
+        image_path = image_path.replace('images', 'labels')
+        image_path = image_path.replace('img1', 'labels')
+        image_path = image_path.replace('JPEGImages', 'labels')
+        image_path = image_path.replace('.jpg', '.txt')
+        image_path = image_path.replace('.png', '.txt')
 
-        line = line.replace('.jpg','.txt')
-        line = line.replace('.png','.txt')
-        print line
-        f2 = open(line)
-        for line in f2.readlines():
-            line = line.rstrip('\n')
-            w,h = line.split(' ')[3:]            
-            #print w,h
-            annotation_dims.append(map(float,(w,h)))
+        annotation_path = image_path
+        print(annotation_path)
+        annotations = open(annotation_path)
+        for line in annotations.read().splitlines():
+            w, h = line.split()[3:]
+            annotation_dims.append(list(map(float, (w, h))))
     annotation_dims = np.array(annotation_dims)
   
-    eps = 0.005
-    
     if args.num_clusters == 0:
-        for num_clusters in range(1,11): #we make 1 through 10 clusters 
-            anchor_file = join( args.output_dir,'anchors%d.txt'%(num_clusters))
+        for num_clusters in range(1, 11):
+            anchor_file = os.path.join(args.output_dir, 'anchors%d.txt' % num_clusters)
 
-            indices = [ random.randrange(annotation_dims.shape[0]) for i in range(num_clusters)]
+            indices = [random.randrange(annotation_dims.shape[0]) for _ in range(num_clusters)]
             centroids = annotation_dims[indices]
-            kmeans(annotation_dims,centroids,eps,anchor_file)
-            print 'centroids.shape', centroids.shape
+            kmeans(annotation_dims, centroids, anchor_file)
+            print('centroids.shape', centroids.shape)
     else:
-        anchor_file = join( args.output_dir,'anchors%d.txt'%(args.num_clusters))
-        indices = [ random.randrange(annotation_dims.shape[0]) for i in range(args.num_clusters)]
+        anchor_file = os.path.join(args.output_dir, 'anchors%d.txt' % args.num_clusters)
+        indices = [random.randrange(annotation_dims.shape[0]) for _ in range(args.num_clusters)]
         centroids = annotation_dims[indices]
-        kmeans(annotation_dims,centroids,eps,anchor_file)
-        print 'centroids.shape', centroids.shape
+        kmeans(annotation_dims, centroids, anchor_file)
+        print('centroids.shape', centroids.shape)
 
-if __name__=="__main__":
-    main(sys.argv)
+
+if __name__ == "__main__":
+    main()
